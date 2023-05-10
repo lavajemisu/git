7fa7da635e9487a714acddf45a0315f048f97a71
lavajemisu <61299571+lavajemisu@users.noreply.github.com>
2023-05-10 21:44:12 +0900

    Add files via upload

---
+++ hw8.py
@@ -0,0 +1,28 @@
+def buy(shopping_bag):
+    print('[구입]')
+    while True:
+        item = input('상품명? ')
+        if item == '':
+            return False
+        quantity = int(input('수량은? '))
+        shopping_bag[item] = quantity
+        print(f'장바구니에 {item} {quantity}개가 담겼습니다.\n')
+
+def show(shopping_bag):
+    print()
+    print('>>> 장바구니 보기:',shopping_bag)
+
+def find(shopping_bag):
+    print('\n[검색]')
+    item = input('장바구니에서 확인하고자 하는 상품은? ')
+    if item in shopping_bag:
+        print(f'{item}은(는) {shopping_bag[item]}개 담겨 있습니다.')
+    else:
+        print(f'장바구니에 {item}은(는) 없습니다.')
+        
+shopping_bag ={}
+while True:
+    if buy(shopping_bag) == False:
+        break
+show(shopping_bag)
+find(shopping_bag)
